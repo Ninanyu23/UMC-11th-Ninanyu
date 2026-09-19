6bdca7bfd298a7bcd62b47a25a78b6832bbb080a
Ninanyu23 <seune2827@gmail.com>
2026-09-19 20:00:33 +0900

    0주차 최종실습

---
+++ Frontend/week0/introduce/script.js
@@ -0,0 +1,6 @@
+const message = document.querySelector("#message");
+const cheerButton = document.querySelector("#cheer-button");
+
+cheerButton.addEventListener("click", function () {
+  message.textContent = "0주차 실습! 앞으로 열심히 해 보아요. 화이팅💪";
+});
