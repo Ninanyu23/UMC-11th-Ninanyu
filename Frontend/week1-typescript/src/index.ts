79625f25359e7e6ce478abecdb34a8b114d4ef20
Ninanyu23 <seune2827@gmail.com>
2026-09-20 21:41:32 +0900

    1주차 미션

---
+++ Frontend/week1-typescript/src/index.ts
@@ -0,0 +1,39 @@
+type MemberRole = "leader" | "member";
+
+type StudyMember = {
+  id: number;
+  name: string;
+  role: MemberRole;
+  githubId?: string; // 선택적 속성
+};
+
+// 서로 다른 정보를 가진 회원 두 명 이상 작성
+const members: StudyMember[] = [
+  { id: 1, name: "니나뉴", role: "leader", githubId: "Ninanyu23" },
+  { id: 2, name: "지수", role: "member" },
+  { id: 3, name: "광수", role: "member", githubId: "gwangsoo" },
+];
+
+// 회원의 id를 입력하면 해당 회원의 이름, 역할, GitHub ID를 출력
+function introduceMember(memberId: number) {
+  const foundMember = members.find((member) => member.id === memberId);
+
+  if (!foundMember) {
+    return "회원을 찾을 수 없습니다.";
+  }
+
+  const displayGithubId = foundMember?.githubId ?? "등록되지 않음";
+
+  return (
+    foundMember.name +
+    "님은 " +
+    foundMember.role +
+    "이며, GitHub ID는 " +
+    displayGithubId +
+    "입니다."
+  );
+}
+
+console.log(introduceMember(1));
+console.log(introduceMember(2));
+console.log(introduceMember(999));
